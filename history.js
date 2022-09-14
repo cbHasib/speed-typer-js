2281af7ddfd1e67880ecd401a39c8d1ad0488b8f
cbHasib <hasibul.pbn@gmail.com>
2022-09-14 21:06:16 +0600

    Fixed: History Shows three columns in large device

--- history.js
+++ history.js
@@ -4,10 +4,9 @@ function addHistory(questionText, timeTaken, errorCount) {
   const newRow = document.createElement("div");
   newRow.classList.add("card");
 
-  console.log(timeTaken)
 
   newRow.innerHTML = `
-  <h3>${questionText}</h3>
+  <h3 class="center">${questionText}</h3>
   <div>
   <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
   <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
@@ -32,7 +31,7 @@ function displayHistory() {
     newRow.classList.add("card");
 
     newRow.innerHTML = `
-  <h3>${test.questionText}</h3>
+  <h3 class="center">${test.questionText}</h3>
   <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
     <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
   `;
