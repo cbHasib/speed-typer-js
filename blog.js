1260bc86b3c1eaaf0cba7e70db605433bf653fb6
cbHasib <hasibul.pbn@gmail.com>
2022-09-14 22:43:14 +0600

    Added all blog post

--- blog.js
+++ blog.js
@@ -11,7 +11,7 @@ const blogPost = [
     id: 02,
     title: "Difference between Global Scope and Block Scope.",
     postBody:
-      "<strong>The Block scope:</strong> <br>So far, we’ve seen variables defined with the var keyword. Var can declare a variable either in the global or local scope. The variables that are declared within the block scope are comparable to local ones. They are available within the block that they are defined.<br><br>The main difference between the local scope and block scope is that the block statements (e.g. if conditions or for loops), don't create a new scope. So the var keyword will not have an effect, because the variables are still in the same scope.<br><br><strong>The Global scope:</strong> <br> A variable exists inside or outside a block. If a variable is declared outside all functions or curly braces ({}), it exists in the global scope. The global variables can be accessed by any line of code in the program, including inside blocks.  ",
+      "<strong>The Block scope:</strong> <br>So far, we have seen variables defined with the var keyword. Var can declare a variable either in the global or local scope. The variables that are declared within the block scope are comparable to local ones. They are available within the block that they are defined.<br><br>The main difference between the local scope and block scope is that the block statements (e.g. if conditions or for loops), do not create a new scope. So the var keyword will not have an effect, because the variables are still in the same scope.<br><br><strong>The Global scope:</strong> <br> A variable exists inside or outside a block. If a variable is declared outside all functions or curly braces ({}), it exists in the global scope. The global variables can be accessed by any line of code in the program, including inside blocks. ",
     postImage: "https://i.ytimg.com/vi/bKH74wo4qS8/maxresdefault.jpg",
   },
   {
@@ -30,23 +30,50 @@ const blogPost = [
   },
 ];
 
-const blogContainer = document.getElementById("blog-container");
-blogContainer.textContent = "";
+const displayPost = () => {
+  const blogContainer = document.getElementById("blog-container");
+  blogContainer.textContent = "";
 
-blogPost.forEach((post) => {
-  const { title, postBody, postImage } = post;
-  const div = document.createElement("div");
-  div.classList.add("col-12", "col-md-6", "col-lg-4");
-  div.innerHTML = `
+  blogPost.forEach((post) => {
+    const { title, postBody, postImage } = post;
+    const div = document.createElement("div");
+    div.classList.add("col-12", "col-md-6", "col-lg-4");
+    div.innerHTML = `
     <div class="card h-100">
         <img src="${postImage}"
             class="card-img-top card-img-size fluid" alt="${title}">
-        <div class="card-body">
+        <div class="card-body text-center">
             <h5 class="card-title text-center">${title}</h5>
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            Launch demo modal
+            <button type="button" class="btn btn-primary mx-auto" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="modalDisplay('${title}', '${postImage}', '${postBody}')">
+            Get Answer
           </button>
         </div>
     </div>`;
-  blogContainer.appendChild(div);
-});
+    blogContainer.appendChild(div);
+  });
+};
+
+const modalDisplay = (title, img, body) => {
+  const modalContainer = document.getElementById("modal-body");
+  modalContainer.textContent = "";
+
+  const div = document.createElement("div");
+  div.classList.add("modal-content");
+  div.innerHTML = `
+    <div class="modal-header">
+      <h5 class="modal-title" id="exampleModalLabel">${title}</h5>
+      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body">
+      <img class="w-100 rounded" src="${img}" alt="${title}">
+      <p class="mt-2">${body}</p>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+    </div>
+  `;
+  modalContainer.appendChild(div);
+};
+
+
+displayPost();
